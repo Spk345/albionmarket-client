e43c1336b6d78d08c37ff3f3d3e7fc334350591a
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-13 16:35:12 -0700

    Trying CGO 0

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 apt-get install -y build-essential mingw-w64
 go get -u github.com/golang/dep/cmd/dep
 dep ensure
@@ -8,7 +10,7 @@ export CGO_CPPFLAGS="-I $GOPATH/src/github.com/regner/albionmarket-client/thirdp
 export CGO_LDFLAGS="-L $GOPATH/src/github.com/regner/albionmarket-client/thirdparty/WpdPack/Lib/x64/"
 export GOOS=windows
 export GOARCH=amd64
-export CGO_ENABLED=1
+export CGO_ENABLED=0
 export CXX=x86_64-w64-mingw32-g++
 export CC=x86_64-w64-mingw32-gcc
 go build -o albionmarket-client.exe -v -x cmd/albionmarket-client/albionmarket-client.go
