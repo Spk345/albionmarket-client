30708d81a383042b2b5ec94b92a387165f15382e
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-13 16:57:46 -0700

    no message

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -10,7 +10,7 @@ export CGO_CPPFLAGS="-I $GOPATH/src/github.com/regner/albionmarket-client/thirdp
 export CGO_LDFLAGS="-L $GOPATH/src/github.com/regner/albionmarket-client/thirdparty/WpdPack/Lib/x64/"
 export GOOS=windows
 export GOARCH=amd64
-export CGO_ENABLED=0
+export CGO_ENABLED=1
 export CXX=x86_64-w64-mingw32-g++
 export CC=x86_64-w64-mingw32-gcc
-go build -o albionmarket-client.exe -a -installsuffix cgo cmd/albionmarket-client/albionmarket-client.go
+go build -o albionmarket-client.exe cmd/albionmarket-client/albionmarket-client.go
