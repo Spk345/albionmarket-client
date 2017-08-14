a9c0aec9f34b798e6fa072409b59f7f7c5522f2f
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-13 17:10:02 -0700

    no message

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -13,4 +13,4 @@ export GOARCH=amd64
 export CGO_ENABLED=1
 export CXX=x86_64-w64-mingw32-g++
 export CC=x86_64-w64-mingw32-gcc
-go build -o albionmarket-client.exe cmd/albionmarket-client/albionmarket-client.go
+go build --ldflags '-extldflags "-static"' -o albionmarket-client.exe cmd/albionmarket-client/albionmarket-client.go
